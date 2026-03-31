05a7a425f34ba73c4ff756c878fbc35f93994504
CristoferMantilla <cristofermantilla1@gmail.com>
2026-03-30 19:09:24 -0500

    first commit

---
+++ Program.py
@@ -0,0 +1,13 @@
+edad : int = 18
+altura : float =1.8
+nom : str = "Carlos"
+ape : str = 'Chan'
+esVerde : bool = False
+
+print(f"{nom} {ape} tiene {edad} años y su carro {"es"if esVerde else "no es"} verde");
+
+#operator ternario
+"""
+es = edad > 18 ? "Mayor edad": "niño";
+es = "Mayor edad" if edad > 18 else "niño"
+"""
